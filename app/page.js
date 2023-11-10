860b06b7f8b0f3c95d87e354fa42673f686e2df3
Kaustuv Karki <karkikaustuv@gmail.com>
2023-11-11 01:04:34 +0545

    Scroll snap checking

--- app/page.js
+++ app/page.js
@@ -3,14 +3,14 @@ import { useRef } from "react";
 import Image from "next/image";
 import MainImage from "../assets/pexels-janko-ferlic-590478.jpg";
 import useScrollSnap from "react-use-scroll-snap";
-import ScrollSnap from "scroll-snap";
+// import ScrollSnap from "scroll-snap";
 
 export default function Home() {
   const container = useRef(null);
   useScrollSnap({ ref: container, duration: 100, delay: 10 });
   return (
     <>
-      <div className="navbar flex justify-between p-5 sticky top-0 z-10 text-white">
+      {/* <div className="navbar flex justify-between p-5 sticky top-0 z-10 text-white">
         <div className="logo">LOGO</div>
         <div className="selections flex gap-x-8 mr-8">
           <h2>Home</h2>
@@ -18,9 +18,9 @@ export default function Home() {
           <h2>Delicaies</h2>
           <h2>Contact</h2>
         </div>
-      </div>
-      <div className="" ref={container}>
-        <div className="h-[100vh] relative">
+      </div> */}
+      <div className="overflow-y-scroll snap-mandatory snap-y" ref={container}>
+        <div className="h-[100vh] relative snap-center">
           <Image
             className="top-0 left-0 h-full w-full object-cover"
             src={MainImage}
@@ -28,7 +28,7 @@ export default function Home() {
           />
           {/* <h1 className="text-black">This is the content</h1> */}
         </div>
-        <div className=" h-[100vh]">
+        <div className=" h-[100vh] snap-center">
           <Image
             className="top-[0] left-0 h-full w-full object-cover"
             src={MainImage}
